7293ad5b42c7e8b4245a93edf6e55eee1405f45b
Michael Nattfalk <michael.nattfalk@consid.se>
2023-11-06 18:45:57 +0100

    Initial commit. 44.232.770p Göteborg

---
+++ Considition2023-Cs/Program.cs
@@ -0,0 +1,92 @@
+﻿using Considition2023_Cs;
+using System.Text.Json.Serialization;
+
+const string apikey = "24857943-542a-4e7d-bbe4-a7a1115b0527";
+
+
+//if (string.IsNullOrWhiteSpace(apikey))
+//{
+//    Console.WriteLine("Configure apiKey");
+//    return;
+//}
+
+//Console.WriteLine($"1: {MapNames.Stockholm}");
+//Console.WriteLine($"2: {MapNames.Goteborg}");
+//Console.WriteLine($"3: {MapNames.Malmo}");
+//Console.WriteLine($"4: {MapNames.Uppsala}");
+//Console.WriteLine($"5: {MapNames.Vasteras}");
+//Console.WriteLine($"6: {MapNames.Orebro}");
+//Console.WriteLine($"7: {MapNames.London}");
+//Console.WriteLine($"8: {MapNames.Linkoping}");
+//Console.WriteLine($"9: {MapNames.Berlin}");
+
+//Console.Write("Select the map you wish to play: ");
+//string option = Console.ReadLine();
+
+//var mapName = option switch
+//{
+//    "1" => MapNames.Stockholm,
+//    "2" => MapNames.Goteborg,
+//    "3" => MapNames.Malmo,
+//    "4" => MapNames.Uppsala,
+//    "5" => MapNames.Vasteras,
+//    "6" => MapNames.Orebro,
+//    "7" => MapNames.London,
+//    "8" => MapNames.Linkoping,
+//    "9" => MapNames.Berlin,
+//    _ => null
+//};
+
+//if (mapName is null)
+//{
+//    Console.WriteLine("Invalid map selected");
+//    return;
+//}
+
+var mapName = MapNames.Goteborg;
+
+HttpClient client = new();
+Api api = new(client);
+MapData mapData = await api.GetMapDataAsync(mapName, apikey);
+GeneralData generalData = await api.GetGeneralDataAsync();
+
+double scoreValue = 0d;
+//while (true)
+//{
+    SubmitSolution solution = new() 
+    {
+        Locations = new()
+    };
+    int i = 0;
+    foreach (KeyValuePair<string, StoreLocation> locationKeyPair in mapData.locations)
+    {
+        StoreLocation location = locationKeyPair.Value;
+        //string name = locationKeyPair.Key;
+        var salesVolume = location.SalesVolume;
+        if (salesVolume > 0)
+        {
+            //var _9100 = Random.Shared.Next(0, 2) * Random.Shared.Next(0, 6);
+            //var _3100 = Random.Shared.Next(0, 2) * Random.Shared.Next(0, 6);
+
+            //if (_3100 + _9100 == 0) continue;
+
+            solution.Locations[location.LocationName] = new PlacedLocations() 
+            { 
+                Freestyle3100Count = i++ % 3 == 0 ? 1 : 0, //_3100,
+                Freestyle9100Count = 1, //_9100,
+            };
+        }
+    }
+
+    GameData score = new Scoring().CalculateScore(string.Empty, solution, mapData, generalData);
+    if (scoreValue < score.GameScore.Total)
+    {
+        scoreValue = score.GameScore.Total;
+        Console.WriteLine($"GameScore: {score.GameScore.Total}");
+    }
+//}
+GameData prodScore = await api.SumbitAsync(mapName, solution, apikey);
+Console.WriteLine($"GameId: {prodScore.Id}");
+
+int a = 1;
+
