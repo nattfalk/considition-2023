e0c0adee9a5f8f384a68335faa9bbf3d3162d63f
nattfalk <michaelnattfalk@gmail.com>
2023-11-07 22:56:37 +0100

    Code cleanup and refactoring

--- Considition2023-Cs/Program.cs
+++ Considition2023-Cs/Program.cs
@@ -1,7 +1,4 @@
-﻿using System.Runtime.InteropServices;
-using Considition2023_Cs;
-using System.Text.Json.Serialization;
-using static System.Formats.Asn1.AsnWriter;
+﻿using Considition2023_Cs;
 
 const string apikey = "24857943-542a-4e7d-bbe4-a7a1115b0527";
 
@@ -10,13 +7,10 @@ Api api = new(client);
 var generalData = await api.GetGeneralDataAsync();
 
 var _3100Capacity = generalData.Freestyle3100Data.RefillCapacityPerWeek;
-var _9100Capacity = generalData.Freestyle9100Data.RefillCapacityPerWeek;
 
 foreach (var mapName in generalData.TrainingMapNames)
 {
     var mapData = await api.GetMapDataAsync(mapName, apikey);
-
-    double scoreValue = 0d;
 
     SubmitSolution solution = new()
     {
@@ -36,32 +30,48 @@ foreach (var mapName in generalData.TrainingMapNames)
 
     solution.Locations = locations;
     var score = new Scoring().CalculateScore(string.Empty, solution, mapData, generalData);
-    Console.WriteLine($"Map: {mapName}, GameScore: {score.GameScore!.Total}");
-    scoreValue = score.GameScore!.Total;
+    Console.WriteLine($"Map: {mapName}, Initial GameScore: {score.GameScore!.Total}");
+    var scoreValue = score.GameScore!.Total;
 
-    Console.WriteLine("** RUN 1");
-    var bestScore = Algo_No9100_Inc3100(locations, ref scoreValue, mapData);
-    Console.WriteLine("** RUN 2");
-    bestScore = Algo_Dec3100(bestScore, ref scoreValue, mapData);
+    Console.WriteLine("** Optimize 1");
+    Optimize(locations, ref scoreValue, mapData, (loc) =>
+    {
+        loc.Freestyle9100Count = 0;
+        loc.Freestyle3100Count += 1;
+    });
+    Console.WriteLine("** Optimize 2");
+    Optimize(locations, ref scoreValue, mapData, (loc) =>
+    {
+        loc.Freestyle3100Count = 0;
+    });
 
-    File.WriteAllText($"{mapName}.json", System.Text.Json.JsonSerializer.Serialize(bestScore));
-    
-    solution.Locations = bestScore;
+    File.WriteAllText($"{mapName}.json", System.Text.Json.JsonSerializer.Serialize(locations));
+
+    solution.Locations = locations
+        .Where(x => x.Value.Freestyle3100Count > 0 || x.Value.Freestyle9100Count > 0)
+        .ToDictionary(x => x.Key, y => y.Value);
+
     score = new Scoring().CalculateScore(string.Empty, solution, mapData, generalData);
-    Console.WriteLine($"Map: {mapName}, GameScore: {score.GameScore!.Total}");
+    Console.WriteLine($"Map: {mapName}, Optimized GameScore: {score.GameScore!.Total}");
     var prodScore = await api.SumbitAsync(mapName, solution, apikey);
     Console.WriteLine($"GameId: {prodScore.Id}");
+    Console.WriteLine();
 }
+return;
 
-Dictionary<string, PlacedLocations> Algo_No9100_Inc3100(Dictionary<string, PlacedLocations> locations, ref double scoreValue, MapData mapData)
+void Optimize(
+    Dictionary<string, PlacedLocations> locations, 
+    ref double scoreValue, 
+    MapData mapData, 
+    Action<PlacedLocations> processLocation)
 {
     Dictionary<string, PlacedLocations> bestScore = new(locations);
     foreach (var location in locations)
     {
         var old3100 = location.Value.Freestyle3100Count;
         var old9100 = location.Value.Freestyle9100Count;
-        location.Value.Freestyle9100Count = 0;
-        location.Value.Freestyle3100Count += 1;
+        
+        processLocation(location.Value);
 
         var solution = new SubmitSolution
         {
@@ -74,47 +84,13 @@ Dictionary<string, PlacedLocations> Algo_No9100_Inc3100(Dictionary<string, Place
         if (scoreValue < score.GameScore!.Total)
         {
             scoreValue = score.GameScore.Total;
-            Console.WriteLine($" - Map: {mapData.MapName}, GameScore: {score.GameScore.Total}");
+            Console.WriteLine($" - GameScore: {score.GameScore.Total}");
 
             old9100 = location.Value.Freestyle9100Count;
             old3100 = location.Value.Freestyle3100Count;
-            bestScore = new Dictionary<string, PlacedLocations>(solution.Locations);
         }
 
         location.Value.Freestyle3100Count = old3100;
         location.Value.Freestyle9100Count = old9100;
     }
-
-    return bestScore;
-}
-
-Dictionary<string, PlacedLocations> Algo_Dec3100(Dictionary<string, PlacedLocations> locations, ref double scoreValue, MapData mapData)
-{
-    Dictionary<string, PlacedLocations> bestScore = new(locations);
-    foreach (var location in locations)
-    {
-        var old3100 = location.Value.Freestyle3100Count;
-        location.Value.Freestyle3100Count = 0;
-
-        var solution = new SubmitSolution
-        {
-            Locations = locations
-                .Where(x => x.Value.Freestyle3100Count > 0 || x.Value.Freestyle9100Count > 0)
-                .ToDictionary(x => x.Key, y => y.Value)
-        };
-
-        var score = new Scoring().CalculateScore(string.Empty, solution, mapData, generalData);
-        if (scoreValue < score.GameScore!.Total)
-        {
-            scoreValue = score.GameScore.Total;
-            Console.WriteLine($" - Map: {mapData.MapName}, GameScore: {score.GameScore.Total}");
-
-            old3100 = location.Value.Freestyle3100Count;
-            bestScore = new Dictionary<string, PlacedLocations>(solution.Locations);
-        }
-
-        location.Value.Freestyle3100Count = old3100;
-    }
-
-    return bestScore;
 }
